9e1248966790d8e70dc8c1340b75678b7688b9b2
Inaba Tewi <soymilk@airmail.cc>
2018-10-31 14:30:31 -0400

    reformat testing output

--- tests.cpp
+++ tests.cpp
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <typeinfo> // typeid(var).name()
 
 #include "print_type.hpp"
@@ -8,15 +9,20 @@ int main() {
   using namespace std;
   
   // Ch.2 Tests
-  cout << two::zero::is_working<int>::value << endl;               // 0
-  cout << two::zero::is_working<int&>::value << endl;              // 1
-  cout << two::zero::is_working<const char&>::value << endl;       // 1
+  cout << "2-0 tests: ";
+  assert(two::zero::is_working<int>::value == 0);
+  assert(two::zero::is_working<int&>::value == 1);
+  assert(two::zero::is_working<const char&>::value == 1);
+  cout << "success" << endl;
 
+  cout << "2-1 tests: " << endl;
   typedef typename two::one::replace_type<const volatile int, int, double>::type one_test0;
   typedef typename two::one::replace_type<const long&, long, float>::type one_test1;
   cout << get_name<one_test0>() << endl;
   cout << get_name<one_test1>() << endl;
 
-  cout << three::zero::binary<101>::value << endl;
-  //cout << three::zero::binary<123>::value << endl; //FAILS COMPILE
+  cout << "3-0 tests: ";
+  assert(three::zero::binary<101>::value != 0);
+  //three::zero::binary<123>::value; //fails compilation as it should
+  cout << "success" << endl;
 }
